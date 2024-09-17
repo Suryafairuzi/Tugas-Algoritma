683c5c2f6572927a041e9d447145c429303b068d
Suryafairuzi <suryafairu6677@gmail.com>
2024-09-17 20:02:19 +0700

    Add files via upload

---
+++ latihan6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float luas,alas,tinggi;
+	cout<<"Inputkan Alas : ";
+	cin>>alas;
+	cout<<"Inputkan Tinggi : ";
+	cin>>tinggi;
+	luas= 0.5 * alas*tinggi;
+	cout << "Luas Segitiga = "<<luas<<endl;
+	return 0;
+	
+}
