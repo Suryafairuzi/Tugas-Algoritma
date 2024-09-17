3fc9d704e42e0baf3c1edd1e2addf38b02886446
Suryafairuzi <suryafairu6677@gmail.com>
2024-09-17 20:01:16 +0700

    Add files via upload

---
+++ latihan5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int luas,panjang,lebar;
+	cout<<"Masukkan Nilai Panjang = ";
+	cin>>panjang;
+	cout<<"Masukkan Nilai Lebar = ";
+	cin>>lebar;
+	luas=panjang=lebar;
+	cout<<"Luas Persegipanjang = "<<luas<<endl;
+	return 0;
+	
+}
